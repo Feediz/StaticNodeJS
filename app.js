ef506133e8356dcb1d2a6b6e3ba87d530b064eb4
Fidel Habtezion <habtezion@gmail.com>
2020-01-03 22:22:26 -0800

    WIP

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const createError = require('http-errors');
 const express = require("express");
 const dotenv = require("dotenv").config();
 
@@ -14,6 +15,24 @@ app.use(router);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use("/static", express.static("public"));
+
+// catch resource not found errors
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
 // start express server
 app.listen(process.env.APP_PORT, () => {
   console.log(`Server listening on port: ${process.env.APP_PORT}`);
